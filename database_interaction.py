70d46512620c40a88baf62f9b43065dce168e242
Vitaliy <42554566+Leosimetti@users.noreply.github.com>
2021-01-31 18:16:03 +0300

    Added database interaction

---
+++ database_interaction.py
@@ -0,0 +1,25 @@
+import sqlite3
+import queue
+
+
+def prepareDB(conn):
+    cursor = conn.cursor()
+
+    cursor.execute("DROP TABLE IF EXISTS signals;")
+    cursor.execute(
+        "CREATE TABLE signals ( id INTEGER PRIMARY KEY AUTOINCREMENT,dateTIme TEXT, deviceType VARCHAR(1), actionType TEXT);")
+
+    conn.commit()
+
+def interact(conn, data):
+    cursor = conn.cursor()
+
+    sas, kek, lol = data.split(" ")
+
+    cursor.execute(f"""
+    INSERT INTO signals
+    (dateTime, deviceType, actionType)
+    VALUES (?, ?, ?);
+    """, [sas, kek, lol])
+
+    conn.commit()
--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from keyboard_tracker import process_keyboard_event, keyboard
 from mouse_tracker import process_mouse_event, mouse
+from database_interaction import *
+
 
 if __name__ == "__main__":
+    conn = sqlite3.connect('signals.sqlite')
+    prepareDB(conn)
+
+    interact(conn, "M kok sas")
+    interact(conn, "M kok sas")
+    interact(conn, "K kok sas")
+    interact(conn, "M kok sas")
+
     keyboard.on_press(callback=process_keyboard_event)
     mouse.hook(callback=process_mouse_event)
     keyboard.wait()
+
+    conn.close()
