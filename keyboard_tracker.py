41d02a289a8276e3cf8969def65ce498137e459a
supersloy <51485198+supersloy@users.noreply.github.com>
2021-02-01 15:51:18 +0300

    Add ckassification of key pressed and connection of keyboard tracking with database

--- keyboard_tracker.py
+++ keyboard_tracker.py
@@ -2,8 +2,20 @@ from datetime import datetime
 import keyboard
 import sqlite3
 
+from database_interaction import insert_data
+
 endpoint = "http://some-url.com"
 
 
 def process_keyboard_event(event: keyboard.KeyboardEvent):
-    print(event.name, datetime.fromtimestamp(event.time))
+    conn = sqlite3.connect('signals.sqlite')
+
+    key = str(event.name)
+    if len(key) == 1:
+        category = "K_TYPING"
+    else:
+        category = "K_NON-TYPING"
+
+    insert_data(conn, f"{datetime.fromtimestamp(event.time)}?K?{category}")
+
+    print(key, datetime.fromtimestamp(event.time))
