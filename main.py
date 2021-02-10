f69dc441da9265c78225f7c64fb4fbe0b33a4786
nikololiahim <olomishcak@outlook.com>
2021-02-10 19:08:36 +0300

    introduced new stuff

--- main.py
+++ main.py
@@ -3,29 +3,34 @@ import time
 
 from db.database_interaction import *
 from db.video_store import *
-from device_tracking.keyboard_tracker import process_keyboard_event, keyboard
-from device_tracking.mouse_tracker import process_mouse_event, mouse
-# from video_tracking.omegamodel import determine_state
-from video_tracking.Keras_face_tracker import determine_state
+from device_tracking.keyboard_tracker import KeyboardTracker
+from device_tracking.mouse_tracker import MouseTracker
+from video_tracking.omegamodel import determine_state
+from queue import Queue
+
+# from video_tracking.Keras_face_tracker import determine_state
 
 
-DEBUG = False
+DEBUG = True
 
 if __name__ == "__main__":
     conn = sqlite3.connect('db/signals.sqlite')
     prepare_signalDB()
     prepare_imageDB()
-
-
-    keyboard.on_press(callback=process_keyboard_event)
-    mouse.hook(callback=process_mouse_event)
-
-    if DEBUG:
-        time.sleep(10)
-        read_signals()
-    # To capture video from webcam.
-    cap = cv2.VideoCapture(0)
-    determine_state(cap)
-
-    keyboard.wait()
     conn.close()
+
+    event_queue = Queue()
+
+    kb_tracker = KeyboardTracker(event_queue, DEBUG)
+    kb_tracker.track()
+
+    mouse_tracker = MouseTracker(event_queue, DEBUG)
+    mouse_tracker.track()
+
+    cap = cv2.VideoCapture(0)
+    # determine_state(cap)
+
+    while True:
+        event = event_queue.get()
+        event.process()
+        event_queue.task_done()
